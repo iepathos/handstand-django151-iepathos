2e42457c93675edbaa52b037f36708c4d16759fd
Glen Baker <iepathos@gmail.com>
2013-04-09 18:04:46 -0700

    Updated google chrome favicon fix

--- handstand-django151-iepathos/urls.py
+++ handstand-django151-iepathos/urls.py
@@ -2,12 +2,13 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic.base import TemplateView
 from django.http import HttpResponseRedirect
+from django.conf import settings
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'', include('social_auth.urls')),
-    url(r'^favicon.ico/$', lambda x: HttpResponseRedirect('/static/ico/favicon.ico')), #google chrome favicon fix
+    url(r'^favicon.ico/$', lambda x: HttpResponseRedirect(settings.STATIC_URL +'ico/favicon.ico')), #google chrome favicon fix
     url(r'^$', TemplateView.as_view(template_name='homepage.html'), name='home'),
     url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
     url(r'^profiles/', include('profiles.urls')),
@@ -16,6 +17,5 @@ urlpatterns = patterns('',
     url(r'^login/$', TemplateView.as_view(template_name='login.html'), name='login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', {}, 'logout',),
     url(r'^associate/$', TemplateView.as_view(template_name='associate.html'), name='associate'),
-
     url(r'^tinymce/', include('tinymce.urls')),
 )
